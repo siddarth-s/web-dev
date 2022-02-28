b960cc48a9aef7cfb61011f340472d3158d6fd0b
Siddarth Sathyanarayanan <44334073+siddarth-s@users.noreply.github.com>
2022-02-27 21:26:37 -0500

    [feat]: index.html, .css, .js added for a5 js lab

--- public/labs/a5/js/index.js
+++ public/labs/a5/js/index.js
@@ -226,3 +226,101 @@ const greeting2  = `Logged in: ${loggedIn2 ? "Yes" : "No"}`;
 console.log(greeting2);
 
 console.log('\n');
+
+const init = () => {
+    console.log('Hello world from jQuery');
+    console.log('\n')
+    /* do the rest of the lab work here */
+    const bindById = $('#bind-by-id');
+    const bindByClass = $('.bind-by-class');
+    console.log('Binding to DOM');
+    console.log(bindById);
+    console.log(bindByClass);
+
+    const changeStyle = $('#change-style');
+    const changeStyle2 = $('.change-style');
+    changeStyle.css('color', 'red');
+    changeStyle2.css('color', 'blue');
+
+    console.log('\n');
+    console.log('Get and set attributes');
+    const getIdAttr = $("#get-id-attr");
+    const id = getIdAttr.attr('id');
+    console.log(id);
+
+    const setClassAttr = $("#set-class-attr");
+    setClassAttr.attr('class', 'class-0');
+
+    const addClass1Example = $("#add-class-1");
+    addClass1Example.addClass('class-1');
+
+    const removeClass1Example = $("#remove-class-1");
+    removeClass1Example.removeClass('class-2')
+
+    const hideMe = $('#hide-me');
+    hideMe.hide();
+
+    const showMe = $("#show-me");
+    showMe.show();
+
+    const newLineItem = $("<li>Line item 1</li>");
+    const anotherLineItem = $("<li>Line item 2</li>");
+
+    const ul = $('#append-new-elements');
+    ul.append(newLineItem);
+    ul.append(anotherLineItem);
+
+    const removeThisLi = $("#remove-this");
+    const emptyThisUl = $("#empty-this");
+    removeThisLi.remove();
+    emptyThisUl.empty();
+
+    const changeThisText =
+        $("#change-this-text");
+    const changeThisHtml =
+        $("#change-this-html");
+    changeThisText.html('New text');
+    changeThisHtml.html(`
+        <li>Line item A</li>
+        <li>Line item B</li>
+        <li>Line item C</li>
+        `);
+
+    const child2 = $("#child-2");
+    const parent1 =
+        child2.parents("#parent");
+    parent1
+        .css('background-color', 'red')
+        .css('color', 'white');
+    const parent = $("#parent");
+    const child = parent.find("#child-2");
+    child.css('background-color', 'blue')
+
+    const handleClick = () => console.log('Handle click');
+    const clickable = $('.clickable');
+    clickable.click(handleClick);
+
+    const handleEventTarget = (event) => {
+        const target = event.target;
+        console.log(target);
+        $(target)
+            .css('background-color', 'blue')
+            .css('color', 'white');
+    }
+    const eventTarget = $("#event-target");
+    eventTarget.click(handleEventTarget);
+
+    let hideBtn, showBtn, hideShowHeader;
+    hideBtn = $('#hide');
+    showBtn = $('#show');
+    hideShowHeader = $('#hide-show');
+    const hideHandler = () => {
+        hideShowHeader.hide();
+    }
+    const showHandler = () => {
+        hideShowHeader.show();
+    }
+    hideBtn.click(hideHandler);
+    showBtn.click(showHandler);
+}
+$(init);
