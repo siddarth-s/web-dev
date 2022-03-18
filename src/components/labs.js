33d8dc95213028f8502d8245e8f204d762e4a017
Siddarth Sathyanarayanan <44334073+siddarth-s@users.noreply.github.com>
2022-03-18 15:06:57 -0400

    [feat]: Navigation

---
+++ src/components/labs.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Labs = () => {
+    return(
+        <h1>Labs</h1>
+    )
+};
+
+export default Labs;
---
+++ src/components/tuiter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Tuiter = () => {
+    return(
+        <h1>Tuiter</h1>
+    )
+};
+
+export default Tuiter;
