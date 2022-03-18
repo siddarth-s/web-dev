2f2b3adf202323302dd4d735f9850b7e2a50e5b2
Siddarth Sathyanarayanan <44334073+siddarth-s@users.noreply.github.com>
2022-03-18 16:46:05 -0400

    [feat]: Classes

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Classes from "./classes";
 
 const Labs = () => {
     return(
         <>
             <h1>Labs</h1>
+            <Classes/>
             <Link to="/hello">
                 Hello
             </Link>
