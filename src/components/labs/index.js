dc0168d11231c8b3ce8617063b68d5c4a1e1851a
Siddarth Sathyanarayanan <44334073+siddarth-s@users.noreply.github.com>
2022-03-19 01:53:18 -0400

    [feat]: Todos in react

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import {Link} from "react-router-dom";
 import Classes from "./classes";
 import Styles from "./styles";
-import conditionalOutput from "./conditional-output";
 import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/todo-list";
 
 const Labs = () => {
     return(
         <>
             <h1>Labs</h1>
+            <TodoList/>
             <ConditionalOutput/>
             <Styles/>
             <Classes/>
