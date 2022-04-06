705f0008ac3f5481050ea28061faf2291b71f5f5
Siddarth Sathyanarayanan <siddarth@Siddarths-MacBook-Pro.local>
2022-04-06 17:46:22 -0400

    [feat]: enabled environment variables

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
+// const TUITS_API = 'https://node-web-dev-server.herokuapp.com/api/tuits';
+
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit);
